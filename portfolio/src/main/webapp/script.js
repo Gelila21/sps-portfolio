4a56c313e23615dbc82604f6f40972e9d425c2c0
Gelila Chaka <gelilchaka@gmail.com>
2022-06-17 00:39:28 +0000

    fixed line 35 in script.ja

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -27,15 +27,14 @@ function addRandomGreeting() {
   greetingContainer.innerText = greeting;
 }
 
-document.getEelementById('getText').addEventListener
-('click', getText);
+document.getEelementById('getText').addEventListener('click', getText);
 
 function getText(){
    fetch("textfile.txt")
    .then(function(response){
-       return reponse.text();
+       return response.text();
    })
    .then(function(data){
-       document.getElementById('output').innerHTML = data;
+       document.getElementById('output').innerText = data;
    })
  }
